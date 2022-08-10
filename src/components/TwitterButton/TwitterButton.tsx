979c52cf9f2d53077b4316ee3b527027c733d521
Hüseyin Averbek <62853117+evirbek@users.noreply.github.com>
2022-08-10 23:15:41 +0300

    first commit

---
+++ src/components/TwitterButton/TwitterButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface TwitterButtonProps {
+    text: string;
+}
+
+const TwitterButton = (props: TwitterButtonProps) => {
+    return <button>Twitter Button</button>
+}
+
+export default TwitterButton
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as TwitterButton } from './TwitterButton';
